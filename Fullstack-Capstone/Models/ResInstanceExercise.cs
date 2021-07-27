d55f6d3a5db2cb86dc5f2a3f89b73d94d907780d
ColtMay <colten_mayberry@pm.me>
2021-07-27 13:22:17 -0500

    model created, adding additional conditions to resinstance repository

---
+++ Fullstack-Capstone/Models/ResInstanceExercise.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Fullstack_Capstone.Models
+{
+    public class ResInstanceExercise
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public int ResInstanceId { get; set; }
+
+        [Required]
+        public int ExerciseId { get; set; }
+
+        [Required]
+        public int Weight { get; set; }
+
+        [Required]
+        public int Difficulty { get; set; }
+    }
+}
--- Fullstack-Capstone/Repositories/ResInstanceRepository.cs
+++ Fullstack-Capstone/Repositories/ResInstanceRepository.cs
@@ -183,6 +183,7 @@ namespace Fullstack_Capstone.Repositories
             }
         }
 
+
     }
 }
 
