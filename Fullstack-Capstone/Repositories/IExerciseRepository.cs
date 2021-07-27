8c4a0027e1ce56e9e597ea85c736f8e6d359daa9
ColtMay <colten_mayberry@pm.me>
2021-07-27 10:51:35 -0500

    created exercise repo interface

---
+++ Fullstack-Capstone/Repositories/IExerciseRepository.cs
@@ -0,0 +1,14 @@
+﻿using Fullstack_Capstone.Models;
+using System.Collections.Generic;
+
+namespace Fullstack_Capstone.Repositories
+{
+    public interface IExerciseRepository
+    {
+        void Add(Exercise exercise);
+        void Delete(int id);
+        List<Exercise> GetAll();
+        Exercise GetById(int id);
+        void Update(Exercise exercise);
+    }
+}
